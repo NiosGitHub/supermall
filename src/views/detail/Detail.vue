c85fdee5c8652b502b3eb05d2b48f52956cfec3a
NiosGitHub <niopeng@qq.com>
2021-08-02 16:01:50 +0800

    0802

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,37 +1,112 @@
 <template>
-  <div class="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav" />
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages" />
+      <detail-base-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detailInfo="detailInfo" @imageLoad="imageLoad" />
+      <detail-param-info :paramInfo="paramInfo" />
+      <detail-comment-info :commentInfo="commentInfo" />
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import DetailShopInfo from "./childComps/DetailShopInfo";
+import DetailCommentInfo from "./childComps/DetailCommentInfo";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+import DetailParamInfo from "./childComps/DetailParamInfo";
 
-import { getDetail } from "network/detail";
+import Scroll from "components/common/scroll/Scroll";
+
+import { getDetail, Goods, Shop, GoodsParam } from "network/detail";
 
 export default {
   name: "Detail",
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailCommentInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    Scroll,
   },
   data() {
     return {
       iid: null,
-      topImages:[]
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {},
+      goodslist: [],
+      themeTops: [],
+      currentIndex: 0,
     };
   },
   created() {
     this.iid = this.$route.params.iid;
+    this.getDetail(this.iid);
+  },
+  methods: {
+    getDetail(iid) {
+      getDetail(iid).then((res) => {
+        console.log(res);
+        const data = res.result;
+        // 1.获取顶部的轮播图片数据
+        this.topImages = data.itemInfo.topImages;
+        // 2.获取商品信息
+        this.goods = new Goods(
+          data.itemInfo,
+          data.columns,
+          data.shopInfo.services
+        );
+        // 3.获取店铺信息
+        this.shop = new Shop(data.shopInfo);
 
-    getDetail(this.iid).then((res) => {
-      this.topImages=res.result.itemInfo.topImages
-    });
+        // 4.商品的详情数据
+        this.detailInfo = data.detailInfo;
+
+        // 5.获取参数信息
+        this.paramInfo = new GoodsParam(
+          data.itemParams.info,
+          data.itemParams.rule
+        );
+
+        //6.获取评论信息
+        this.commentInfo = data.rate.list[0];
+      });
+    },
+    imageLoad() {
+      this.$refs.scroll.refresh();
+    },
   },
 };
 </script>
 
 <style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+
+.deatil-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+
+.content {
+  height: calc(100% - 44px);
+  overflow: hidden;
+}
 </style>
